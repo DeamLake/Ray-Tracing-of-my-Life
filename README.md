c4665cec3455b8dab794c042ae2ab154b03f253e
deamlake <1115709562@qq.com>
2022-04-28 15:58:09 +0800

    finish One weekend

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Ray-Tracing-of-my-Life
 Ray Tracing in One Weekend/The next Week/The Rest of my life
+
+## TODO
+- [ ] Tracing in One Weekend
+    - [x] Code
+    - [ ] more docs
+- [ ] The next Week
+- [ ] The Rest of my life
+## Notes
+You can find more info at [_Ray Tracing in One Weekend_](https://raytracing.github.io/books/RayTracingInOneWeekend.html)
+
+
--- RayTracingInOneWeekend/Utils/color.h
+++ RayTracingInOneWeekend/Utils/color.h
@@ -25,10 +25,21 @@ public:
         fclose(imageFile);
     }
 
-    void write_color(const color& pixel_color) {
-        unsigned char cr = static_cast<unsigned char>(255.999 * pixel_color.x());
-        unsigned char cg = static_cast<unsigned char>(255.999 * pixel_color.y());
-        unsigned char cb = static_cast<unsigned char>(255.999 * pixel_color.z());
+    void write_color(const color& pixel_color, int samples_per_pixel) {
+        auto r = pixel_color.x();
+        auto g = pixel_color.y();
+        auto b = pixel_color.z();
+
+        // Divide the color by the number of samples and gamma-correct for gamma=2.0.
+        auto scale = 1.0 / samples_per_pixel;
+        r = sqrt(scale * r);
+        g = sqrt(scale * g);
+        b = sqrt(scale * b);
+
+        unsigned char cr = static_cast<unsigned char>(256 * clamp(r, 0.0, 0.999));
+        unsigned char cg = static_cast<unsigned char>(256 * clamp(g, 0.0, 0.999));
+        unsigned char cb = static_cast<unsigned char>(256 * clamp(b, 0.0, 0.999));
+
         image_data[idx] = cr;
         image_data[idx + 1] = cg;
         image_data[idx + 2] = cb;
