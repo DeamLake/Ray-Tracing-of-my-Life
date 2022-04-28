3b735136c0ae55a7714caed7616e647e59615d2a
deamlake <1115709562@qq.com>
2022-04-28 11:34:08 +0800

    Add OutputHelper

---
+++ RayTracingInOneWeekend/Utils/color.h
@@ -0,0 +1,42 @@
+#pragma once
+#include<vector>
+#include "vec3.h"
+#include "svpng.inc"
+
+class OutputHelper {
+public:
+    OutputHelper(const int width, const int height, const char* name) 
+        :image_width(width), image_height(height), image_name(name) , idx(0)
+    {
+        image_data = std::vector<unsigned char>(image_height * image_width * 3);
+    }
+
+    ~OutputHelper()
+    {
+        fopen_s(&imageFile, image_name, "wb");
+        if (imageFile == NULL) {
+            perror("ERROR: Cannot open output file");
+            exit(EXIT_FAILURE);// Dont let exception get out of the destructor
+        }
+        svpng(imageFile, image_width, image_height, image_data.data(), 0);
+        fclose(imageFile);
+    }
+
+    void write_color(color pixel_color) {
+        unsigned char cr = static_cast<unsigned char>(255.999 * pixel_color.x());
+        unsigned char cg = static_cast<unsigned char>(255.999 * pixel_color.y());
+        unsigned char cb = static_cast<unsigned char>(255.999 * pixel_color.z());
+        image_data[idx]        = cr;
+        image_data[idx + 1] = cg;
+        image_data[idx + 2] = cb;
+        idx += 3;
+    }
+
+private:
+    const int image_width;
+    const int image_height;
+    const char* image_name;
+    int idx;
+    std::vector<unsigned char> image_data;
+    FILE* imageFile;
+};
--- RayTracingInOneWeekend/main.cpp
+++ RayTracingInOneWeekend/main.cpp
@@ -1,45 +1,23 @@
 #include<iostream>
-#include<vector>
-#include "utils/svpng.inc"
-using namespace std;
+#include "Utils/color.h"
+#include "Utils/vec3.h"
 
-void output(const int image_width, const int image_height, const char* image_name) 
-{
-    FILE* imageFile;
-    fopen_s(&imageFile, image_name, "wb");
-    if (imageFile == NULL) {
-        perror("ERROR: Cannot open output file");
-        exit(EXIT_FAILURE);
-    }
+int main() {
+    const int   image_width     = 256;
+    const int   image_height    = 256;
+    const char* image_name   = "test.png";
 
-    vector<unsigned char> pic(image_height * image_width * 3);
-    int idx = 0;
-
-    for (int i= image_height - 1; i >= 0; --i) {
-        for (int j = 0; j <image_width; ++j) {
+    OutputHelper helper(image_width, image_height, image_name);
+    for (int i = image_height - 1; i >= 0; --i) {
+        std::cerr << "\rScanlines remaining: " << i << ' ' << std::flush;
+        for (int j = 0; j < image_width; ++j) {
             auto r = double(j) / (image_width - 1);
             auto g = double(i) / (image_width - 1);
             auto b = 0.25;
 
-            int ir  = static_cast<int>(255.999 * r);
-            int ig = static_cast<int>(255.999 * g);
-            int ib = static_cast<int>(255.999 * b);
-
-            unsigned char cr = static_cast<unsigned char>(ir);
-            unsigned char cg = static_cast<unsigned char>(ig);
-            unsigned char cb = static_cast<unsigned char>(ib);
-
-            pic[idx]        = cr;
-            pic[idx + 1] = cg;
-            pic[idx + 2] = cb;
-            idx += 3;
+            color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+            helper.write_color(pixel_color);
         }
     }
-
-    svpng(imageFile, image_width, image_height, pic.data(), 0);
-    fclose(imageFile);
-}
-
-int main() {
-    output(256, 256, "test.png");
+    std::cerr << "\nDone.\n";
 }
